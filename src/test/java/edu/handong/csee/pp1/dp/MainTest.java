188251b4504208f5f246ef1ccabd54bbcea1b723
han <han@172.17.220.78>
2018-10-23 21:38:00 +0900

    done task num 2

--- src/test/java/edu/handong/csee/pp1/dp/MainTest.java
+++ src/test/java/edu/handong/csee/pp1/dp/MainTest.java
@@ -13,14 +13,17 @@ public class MainTest {
 		// TASK1 TODO 아래 args적절히 수정. MetricCollector.java를 완성하면, arff파일에 다양한 값들을 볼 수 있음.
 		// (1) 첫번째 파라미터: 현재 수정하고자 하는 PP1-DP의 경로를 값으로 할당 (본인의 경로에 맞게 적당히 수정)
 		// (2) 두번째 파라미터: 첫번째 파라미터에 넣은 git 레포지토리에서 최종 생성된 training data file이름. data/로 시작해야 함.
+		
 		String[] args = {"/Users/han/Desktop/java/project3_practiveone/PP1-DP","data/PP1-DP.arff"};
 		// String[] args = {"/use/this/style/for/your/path"}; // for mac users
 		
+		// String[] args = {"/Users/han/desktop/java/project3_practiveone/techjobs-oo-master","data/FirstSmalljavaProject.arff"};
 		
 		// for my path: { /Users/han/Desktop/java/project3_practiveone }
 		// my path num2
 		// String[] args = {"/Users/han/Desktop/java/project3_practiveone"}; // for mac users
-		
+		///Users/han/desktop/java/project3_practiveone/techjobs-oo-master
+		// /Users/han/desktop/java/project3_practiveone/java
 		
 		assertEquals(args.length,2);
 		
@@ -32,5 +35,19 @@ public class MainTest {
 		// TASK2 TODO GitHub에서 작은 규모의 자바 프로젝트를 2개를 찾아, arff파일을 생성하시오. 파일이름은 [project이름].arff data디렉터리에 저장되게 하고
 		// 생성이 된 후 commit/push해야 함.
 		// 위 test main을 참고하여 실행하면 됨.
+		String[] args1 = {"/Users/han/desktop/java/project3_practiveone/techjobs-oo-master","data/FirstSmalljavaProject.arff"};
+		Main.main(args1);
+		
 	}
+	
+	@Test
+	public void testForTasks2AnotherOne() {
+		// TASK2 TODO GitHub에서 작은 규모의 자바 프로젝트를 2개를 찾아, arff파일을 생성하시오. 파일이름은 [project이름].arff data디렉터리에 저장되게 하고
+		// 생성이 된 후 commit/push해야 함.
+		// 위 test main을 참고하여 실행하면 됨.
+		String[] args2 = {"/Users/han/desktop/java/project3_practiveone/java","data/SecondSmalljavaProject.arff"};
+		Main.main(args2);
+		
+	}	
+	
 }
